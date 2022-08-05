0738943075280549992c25cebb955106de2b3b82
Williams V <wibrvallejoma@gmail.com>
2022-08-05 00:13:09 -0500

    feat: add initial setState

--- 01-monsters-rolodex/src/App.js
+++ 01-monsters-rolodex/src/App.js
@@ -18,7 +18,11 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>Hi, { this.state.userName }</p>
-          <button>
+          <button
+            onClick={() => {
+              this.setState({userName: 'Will :D'})
+            }}
+          >
             Change name
           </button>
         </header>
