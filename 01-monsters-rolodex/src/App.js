91d5437058fc195ec0fc1a2aa0a1d2119fb6ca9c
Williams V <wibrvallejoma@gmail.com>
2022-08-05 00:06:23 -0500

    feat: add initial component state

--- 01-monsters-rolodex/src/App.js
+++ 01-monsters-rolodex/src/App.js
@@ -4,22 +4,23 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render () {
+  constructor() {
+    super();
+
+    this.state = {
+      userName: 'User name'
+    }
+  }
+
+  render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <p>Hi, { this.state.userName }</p>
+          <button>
+            Change name
+          </button>
         </header>
       </div>
     );
