bb421c8113bc7834b3fffbb07384fc00551eb978
Williams V <wibrvallejoma@gmail.com>
2022-08-28 23:11:55 -0500

    feat: 32-33: mapping array to elements

--- 01-monsters-rolodex/src/App.js
+++ 01-monsters-rolodex/src/App.js
@@ -8,41 +8,35 @@ class App extends Component {
     super();
 
     this.state = {
-      name: {
-        firstName: 'Jose',
-        lastName: 'Rodriguez'
-      },
-      company: 'Company name'
+      monsters : [
+        {
+          id: '1',
+          name: 'Jack'
+        },
+        {
+          id: '2',
+          name: 'Joan'
+        },
+        {
+          id: '3',
+          name: 'Abigail'
+        },
+        {
+          id: '4',
+          name: 'Linda'
+        }
+      ]
     }
   }
 
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Hi, { this.state.name.firstName }, { this.state.name.lastName } <br></br>
-            I work at { this.state.company }
-          </p>
-          <button
-            onClick={() => {
-              // Set state could have 2 arguments. A function and a callback.
-              // To get a more sync behavior instead of async.
-              this.setState((state, props) => {
-                // Function
-                return {
-                  name: { firstName: 'UFirstName', lastName: 'ULastName' }
-                }
-              }, () => {
-                // Callback when function is done.
-                console.log(this.state);
-              });
-            }}
-          >
-            Change name
-          </button>
-        </header>
+        {
+          this.state.monsters.map((monster) => {
+            return <h1 key={ monster.id }>{ monster.name }</h1>
+          })
+        }
       </div>
     );
   }
