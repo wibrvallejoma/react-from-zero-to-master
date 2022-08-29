8a37ed39d77a73f24f2cc55fd58356e1b90bc635
Williams V <wibrvallejoma@gmail.com>
2022-08-28 23:53:00 -0500

    search filter and restoring data

--- 01-monsters-rolodex/src/App.js
+++ 01-monsters-rolodex/src/App.js
@@ -1,6 +1,5 @@
 import { Component } from 'react';
 
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -8,23 +7,44 @@ class App extends Component {
     super();
 
     this.state = {
-      monsters : []
+      monsters: [],
+      searchField: ''
     }
   }
 
   componentDidMount() {
     fetch('https://jsonplaceholder.typicode.com/users')
       .then(response => response.json())
-      .then(users => this.setState(() => {
-        return {monsters: users}
-      }));
+      .then(users => this.setState(
+        () => {
+        return {monsters: users};
+        },
+        () => {
+          console.log(this.state);
+        }
+      ));
   }
 
   render() {
+    const filteredMonsters = this.state.monsters.filter((monster) => {
+      return monster.name.toLowerCase().includes(this.state.searchField)
+    });
+
     return (
       <div className="App">
+        <input
+          className='search-box'
+          type='search'
+          placeholder='Search monsters'
+          onChange={(event) => {
+            const searchField = event.target.value.toLowerCase();
+            this.setState(() => {
+              return { searchField }
+            });
+          }}    
+        />
         {
-          this.state.monsters.map((monster) => {
+          filteredMonsters.map((monster) => {
             return <h1 key={ monster.id }>{ monster.name }</h1>
           })
         }
