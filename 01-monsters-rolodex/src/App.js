010139325b939db1b92b2ff83f07e2d0d5990bf8
Williams V <wibrvallejoma@gmail.com>
2022-08-29 00:03:57 -0500

    refactor: optimization

--- 01-monsters-rolodex/src/App.js
+++ 01-monsters-rolodex/src/App.js
@@ -25,9 +25,20 @@ class App extends Component {
       ));
   }
 
+  onSearchChange = (event) => {
+    const searchField = event.target.value.toLowerCase();
+    this.setState(() => {
+      return { searchField }
+    });
+  }
+
   render() {
-    const filteredMonsters = this.state.monsters.filter((monster) => {
-      return monster.name.toLowerCase().includes(this.state.searchField)
+
+    const { monsters, searchField } = this.state;
+    const { onSearchChange } = this;
+
+    const filteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLowerCase().includes(searchField)
     });
 
     return (
@@ -36,12 +47,7 @@ class App extends Component {
           className='search-box'
           type='search'
           placeholder='Search monsters'
-          onChange={(event) => {
-            const searchField = event.target.value.toLowerCase();
-            this.setState(() => {
-              return { searchField }
-            });
-          }}    
+          onChange={onSearchChange}    
         />
         {
           filteredMonsters.map((monster) => {
