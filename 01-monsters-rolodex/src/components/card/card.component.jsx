9c867db9c779de80e10f9ad22fb882719890903c
Williams V <wibrvallejoma@gmail.com>
2022-09-03 00:17:26 -0500

    52: add css and title to app

--- 01-monsters-rolodex/src/components/card/card.component.jsx
+++ 01-monsters-rolodex/src/components/card/card.component.jsx
@@ -5,15 +5,14 @@ class Card extends Component {
 
   render() {
     const { id, alt, src, title, subtitle } = this.props;
-
     return (
       <div className="card-container" key={id}>
         <img
           alt={alt}
           src={src}
         />
-        <h2>{title}</h2>
-        <h3>{subtitle}</h3>
+        <h2 className="card-title">{title}</h2>
+        <h3 className="card-title">{subtitle}</h3>
       </div>
     )
   }
