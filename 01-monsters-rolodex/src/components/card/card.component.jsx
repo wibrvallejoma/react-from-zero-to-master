cbb7d3ef5af427afe90cf31012e023fa99d0807b
Williams Vallejo <williams.b.vallejo.mariscal@intel.com>
2022-09-09 12:33:37 -0500

    61: update card-component to functional component

--- 01-monsters-rolodex/src/components/card/card.component.jsx
+++ 01-monsters-rolodex/src/components/card/card.component.jsx
@@ -1,21 +1,16 @@
-import { Component } from "react";
-
 import './card.styles.css'
-class Card extends Component {
 
-  render() {
-    const { id, alt, src, title, subtitle } = this.props;
-    return (
-      <div className="card-container" key={id}>
-        <img
-          alt={alt}
-          src={src}
-        />
-        <h2 className="card-title">{title}</h2>
-        <h3 className="card-title">{subtitle}</h3>
-      </div>
-    )
-  }
-}
+const Card = ({ id, alt, src, title, subtitle }) => {
+  return (
+    <div className="card-container" key={id}>
+      <img
+        alt={alt}
+        src={src}
+      />
+      <h2 className="card-title">{title}</h2>
+      <h3 className="card-title">{subtitle}</h3>
+    </div>
+  )
+};
 
 export default Card;
