cca19731b4ac93ace5f6b866fe28498a5c73a21b
Williams Vallejo <williams.b.vallejo.mariscal@intel.com>
2022-09-09 12:27:07 -0500

    61: update card-list-component to functional component

--- 01-monsters-rolodex/src/components/card-list/card-list.component.jsx
+++ 01-monsters-rolodex/src/components/card-list/card-list.component.jsx
@@ -1,28 +1,23 @@
-import { Component } from "react";
 import Card from "../card/card.component";
-
 import './card-list.styles.css'
-class CardList extends Component {
 
-  render() {
-    const { monsters } = this.props;
-
-    return (
-      <div className='card-list'>
-        {monsters.map((monster) => {
-          const { name, email, id } = monster;
-          return (
-            <Card
-              alt={`monster ${name}`}
-              src={`https://robohash.org/${id}?set=set4&size=180x180`}
-              title={name}
-              subtitle={email}
-            />
-          )
-        })}
-      </div>
-    )
-  }
-}
+const CardList = ({ monsters }) => {
+  // If a function component does not have a return,variables or other functions, It will do an implicit return.
+  return (
+    <div className='card-list'>
+      {monsters.map((monster) => {
+        const { name, email, id } = monster;
+        return (
+          <Card
+            alt={`monster ${name}`}
+            src={`https://robohash.org/${id}?set=set4&size=180x180`}
+            title={name}
+            subtitle={email}
+          />
+        )
+      })}
+    </div>
+  )
+};
 
 export default CardList;
