48384da372a747844b78134fae9241af74b9a632
Williams Vallejo <williams.b.vallejo.mariscal@intel.com>
2022-09-09 12:50:49 -0500

    61: update search-box to functional component

--- 01-monsters-rolodex/src/components/search-box/search-box.component.jsx
+++ 01-monsters-rolodex/src/components/search-box/search-box.component.jsx
@@ -1,21 +1,14 @@
-import { Component } from "react";
-
 import './search-box.styles.css';
 
-class SearchBox extends Component {
-
-  render() {
-    const { className, placeholder, onChangeHandler } = this.props;
-
-    return (
-      <input
-        className={`search-box ${className}`}
-        type='search'
-        placeholder={placeholder}
-        onChange={onChangeHandler}    
-      />
-    )
-  }
-}
+const SearchBox = ({ className, placeholder, onChangeHandler }) => {
+  return (
+    <input
+      className={`search-box ${className}`}
+      type='search'
+      placeholder={placeholder}
+      onChange={onChangeHandler}
+    />
+  )
+};
 
 export default SearchBox;
