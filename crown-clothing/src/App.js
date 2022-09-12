6c422a7da77f22bdc80dadc99b38539df3a17a9e
Williams Vallejo <williams.b.vallejo.mariscal@intel.com>
2022-09-11 20:45:52 -0500

    feat: add navigation to app.js

--- crown-clothing/src/App.js
+++ crown-clothing/src/App.js
@@ -4,14 +4,17 @@ import { Routes, Route } from 'react-router-dom';
 // Routes
 import Home from './routes/home/home';
 import Shop from './routes/shop/shop';
+import Navigation from './components/navigation/navigation.component';
 
 const App = () => {
   return (
+    <div className='App'>
+    <Navigation />
     <Routes>
-      <Route path='/' element={<Home />}>
-        <Route path='shop' element={<Shop />} />
-      </Route>
+      <Route index element={<Home />} />
+      <Route path='shop' element={<Shop />} />
     </Routes>
+    </div>
   );
 };
 
