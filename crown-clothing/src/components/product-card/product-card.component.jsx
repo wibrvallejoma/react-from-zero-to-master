b1b96e429f2b12541c62f8d16c735514393e48ed
Williams Vallejo <williams.b.vallejo.mariscal@intel.com>
2022-09-18 12:14:49 -0500

    feat: add product card

---
+++ crown-clothing/src/components/product-card/product-card.component.jsx
@@ -0,0 +1,23 @@
+// Components
+import Button from '../button/button.component';
+
+// Styles
+import './product-card.styles.scss';
+
+
+const ProductCard = ({ product }) => {
+  const { name, price, imageUrl } = product;
+
+  return (
+    <div className='product-card-container'>
+      <img src={imageUrl} alt={`${name}`} />
+      <div className='footer'>
+        <span className='name'>{name}</span>
+        <span className='price'>{price}</span>
+      </div>
+      <Button buttonType='inverted'>ADD TO CART</Button>
+    </div>
+  )
+};
+
+export default ProductCard;
