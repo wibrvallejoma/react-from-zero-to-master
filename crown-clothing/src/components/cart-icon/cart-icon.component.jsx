bf0ac95a5ae32389981e7417ab323f02047d6f65
Williams Vallejo <williams.b.vallejo.mariscal@intel.com>
2022-09-21 19:20:43 -0500

    feat: add cart counter

--- crown-clothing/src/components/cart-icon/cart-icon.component.jsx
+++ crown-clothing/src/components/cart-icon/cart-icon.component.jsx
@@ -6,14 +6,14 @@ import { CartContext } from "../../contexts/cart.context";
 import './cart-icon.styles.scss';
 
 const CartIcon = () => {
-  const { IsCartOpen, setIsCartOpen } = useContext(CartContext);
+  const { IsCartOpen, setIsCartOpen, cartCount } = useContext(CartContext);
 
   const toogleIsCartOpen = () => setIsCartOpen(!IsCartOpen);
 
   return (
     <div className='cart-icon-container' onClick={toogleIsCartOpen}>
       <ShoppingIcon className='shopping-icon' />
-      <span className='item-count'>0</span>
+      <span className='item-count'>{cartCount}</span>
     </div>
   )
 };
