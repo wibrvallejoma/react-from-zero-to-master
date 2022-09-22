589bfd0702196810073d6323334196f4f95bbbd8
Williams Vallejo <williams.b.vallejo.mariscal@intel.com>
2022-09-21 19:11:29 -0500

    feat: add cart context, item and dropdown

--- crown-clothing/src/components/product-card/product-card.component.jsx
+++ crown-clothing/src/components/product-card/product-card.component.jsx
@@ -1,3 +1,7 @@
+import { useContext } from 'react';
+
+import { CartContext } from '../../contexts/cart.context';
+
 // Components
 import Button from '../button/button.component';
 
@@ -7,6 +11,9 @@ import './product-card.styles.scss';
 
 const ProductCard = ({ product }) => {
   const { name, price, imageUrl } = product;
+  const { addItemToCart } = useContext(CartContext);
+
+  const addProductToCart = () => addItemToCart(product);
 
   return (
     <div className='product-card-container'>
@@ -15,7 +22,7 @@ const ProductCard = ({ product }) => {
         <span className='name'>{name}</span>
         <span className='price'>{price}</span>
       </div>
-      <Button buttonType='inverted'>ADD TO CART</Button>
+      <Button buttonType='inverted' onClick={addProductToCart}>ADD TO CART</Button>
     </div>
   )
 };
